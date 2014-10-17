cb30606529d84ee59ae291579f5885940e4b84db
mgiulio <giulio.mainardi@gmail.com>
2014-10-17 15:17:32 +0200

    Fix and improve js Stripe token handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@ jQuery(function($) {
 		 
 	Stripe.setPublishableKey(mgStripeCfg.publishableKey);
 		
-    var 		
-		token = undefined,
+    var
+		tokenInfo = null,
 		checkoutForm = $('form.checkout'),
 		errorBox = (function() {
 			var 
@@ -25,7 +25,7 @@ jQuery(function($) {
 				}
 			};
 		})(),
-		log = !mgStripeCfg.logging || !console ? function() {} : function() { console.log.apply(console, ['mg WC Stripe: '].concat(Array.prototype.slice.call(arguments, 0))); }
+		log = !mgStripeCfg.logging || !console ? function() {} : function() { console.log.apply(console, ['mg WC Stripe:'].concat(Array.prototype.slice.call(arguments, 0))); }
 	;
 
 	checkoutForm.on(
@@ -33,60 +33,104 @@ jQuery(function($) {
 		getStripeToken
 	);
 	
-	function getStripeToken() {
-		errorBox.hide();
-
-		if (token) {
-			log('Token found');
-			$('<input type="hidden" name="stripe_token">').val(token).appendTo(checkoutForm);
+	function tokenNeeded() {
+		if (tokenInfo == null)
+			return true;
+			
+		try {
+			var tokenArgs = createTokenArgs();
+			return tokenInfo.tokenArgs !== JSON.stringify(tokenArgs);
+		}
+		catch(e) {
 			return true;
 		}
+	}
+	
+	function createTokenArgs() {
+		var errors = [];
 		
-		blockUI();
-			
 		var cardNumber = $('#' + mgStripeCfg.gatewayId + '-card-number').val();
 		if (!$.payment.validateCardNumber(cardNumber))
-			errorBox.push('Invalid credit card number');
+			errors.push('Invalid credit card number');
 		
 		var 
 			expiryString = $('#' + mgStripeCfg.gatewayId + '-card-expiry').val(),
 			expiryDate = $.payment.cardExpiryVal(expiryString)
 		;
 		if (!$.payment.validateCardExpiry(expiryDate.month, expiryDate.year))
-			errorBox.push('Invalid credit card expiry date');
+			errors.push('Invalid credit card expiry date');
 			
 		var cvc = $('#' + mgStripeCfg.gatewayId + '-card-cvc').val();
 		if (cvc.length > 0)
 			if (!$.payment.validateCardCVC(cvc))
-				errorBox.push('Invalid credit card CVC');
-		
-		if (errorBox.errors()) {
-			errorBox.show();
-			unblockUI();
-			return false;
+				errors.push('Invalid credit card CVC');
+	
+		if (errors.length > 0) {
+			var ex = new Error();
+			ex.errors = errors;
+			throw ex;
 		}
 		
-		var tokenCreationArgs = {
+		var tokenArgs = {
 			number: cardNumber, 
 			exp_month: expiryDate.month,
 			exp_year: expiryDate.year
 		};
 		if (cvc.length > 0)
-			tokenCreationArgs.cvc = cvc;
+			tokenArgs.cvc = cvc;
+
+		return tokenArgs;
+	}
+	
+	function getStripeToken() {
+		errorBox.hide();
+
+		if (!tokenNeeded()) {
+			log('We have an updated token:', tokenInfo);
+			
+			// Make sure it is in the checkout form
+			var input;
+			if ((input = $('input[name=stripe_token]')).length > 0)
+				input.val(tokenInfo.token);
+			else
+				$('<input name="stripe_token">').val(tokenInfo.token).appendTo(checkoutForm);
+			
+			return true;
+		}
 		
-		log('Asking Stripe for a token...');
-		Stripe.card.createToken(tokenCreationArgs, stripeResponseHandler);
+		blockUI();
+		
+		try {
+			tokenArgs = createTokenArgs();
+		}
+		catch (e) {
+			for (var i = 0; i < e.errors.length; i++)
+				errorBox.push(e.errors[i]);
+			errorBox.show();
+			
+			unblockUI();
+			
+			return false;
+		}
+		
+		log('Asking Stripe for a token for card: ', tokenArgs);
+		Stripe.card.createToken(tokenArgs, stripeResponseHandler.bind(null, JSON.stringify(tokenArgs)));
 		
 		return false;
     }
 	
-	function stripeResponseHandler(status, response) {
-		log('...Stripe replied with status: ' + status + ' and response : ', response);
+	function stripeResponseHandler(tokenArgs, status, response) {
+		log('Stripe replied with status ' + status + ' and response ', response);
 		
 		unblockUI();
 
 		if (status == 200) {
-			token = response.id;
+			tokenInfo = {
+				token: response.id,
+				tokenArgs: tokenArgs
+			};
+			log('Token created:', tokenInfo);
+			
 			checkoutForm.submit();
 		} else
 			errorBox.push(response.error.message).show();
