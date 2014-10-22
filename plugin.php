ec6b76281b122d4ea08e9e0c1f272006d6f404d0
mgiulio <giulio.mainardi@gmail.com>
2014-10-22 15:36:50 +0200

    v1.0-beta

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: mg wc Stripe
 Plugin URI: http://mgiulio.info/projects/mg-wc-stripe
 Description: Stripe payment gateway for WooCommerce
-Version: 1.0-beta-pvt
+Version: 1.0-beta
 Author: Giulio 'mgiulio' Mainardi
 Author URI: http://mgiulio.info
 License: GPL2
